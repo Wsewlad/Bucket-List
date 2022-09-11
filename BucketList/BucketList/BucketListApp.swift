d1484cff3d84e0cbc59c594f8f6eb93081eb4ddb
vfil <wladyslawfil@gmail.com>
2022-09-11 21:01:06 +0300

    Create project

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by  Vladyslav Fil on 11.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
